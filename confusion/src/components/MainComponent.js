5e19701eb80440e79bd15a696b02c53626d6ac0e
Dakshina Ranmal <76484102+dakshina99@users.noreply.github.com>
2022-05-07 23:38:47 +0530

    Assignment 2

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -9,6 +9,8 @@ import Contact from './ContactComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
+import DishDetail from "./DishdetailComponent";
+import About from "./AboutComponent";
 
 class Main extends Component {
 
@@ -38,13 +40,22 @@ class Main extends Component {
             );
           }
 
+          const DishWithId = ({match}) => {
+            return(
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+                  comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+            );
+          };
+
         return (
             <div>
                 <Header />
 
                 <Switch>
                     <Route path='/home' component={HomePage} />
+                    <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exat path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
